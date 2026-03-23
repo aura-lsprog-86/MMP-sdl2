1d987824c329e39b00acf5f83e05a363a5bda31e
Aura Lesse Programmer <aurasys.7608@zohomail.com>
2026-03-22 20:20:55 -0700

    Add preliminary Docker recipe
    This covers main SDL2 compilation.

---
+++ 02-build-sources.sh
@@ -0,0 +1,23 @@
+#/bin/sh
+
+# Build SDL2 for MM(P)
+# Instructions based on: https://github.com/Luspin/sdl2-MMP?tab=readme-ov-file#clone--build-steward-fusdl2
+
+cd sdl2-master/
+
+find . -type f -name "*.sh" -exec chmod +x {} \;
+
+# Build steps provided by the SDL2 repo:
+make -j"$(nproc)" cfg CFLAGS="-Wno-psabi" CXXFLAGS="-Wno-psabi"
+# - Expecting:
+# -- Build files have been written to: /app/sdl2-master/sdl2/swiftshader/build
+
+make -j"$(nproc)" gpu CFLAGS="-Wno-psabi" CXXFLAGS="-Wno-psabi"
+ls swiftshader/build/lib*
+# - Expecting:
+# swiftshader/build/libEGL.so  swiftshader/build/libGLESv2.so
+
+make -j"$(nproc)" sdl2 CFLAGS="-Wno-psabi" CXXFLAGS="-Wno-psabi"
+ls sdl2/build/.libs/libSDL2-2.0.so.0*
+# - Expecting:
+# sdl2/build/.libs/libSDL2-2.0.so.0  sdl2/build/.libs/libSDL2-2.0.so.0.18.2
---
+++ Dockerfile
@@ -0,0 +1,34 @@
+FROM ubuntu:24.04
+
+RUN apt-get update && apt-get upgrade -y --no-install-recommends \
+  build-essential \
+  cmake \
+  ninja-build \
+  autoconf \
+  automake \
+  libtool \
+  pkg-config \
+  gettext \
+  m4 \
+  perl \
+  python3 \
+  unzip \
+  patch \
+  wget \
+  libarchive-tools \
+  ca-certificates \
+  && rm -rf /var/lib/apt/lists/*
+
+WORKDIR /app
+
+COPY 01-prepare-deps.sh .
+
+RUN chmod +x 01-prepare-deps.sh \
+  && ./01-prepare-deps.sh
+  
+COPY 02-build-sources.sh .
+
+RUN chmod +x 02-build-sources.sh \
+  && ./02-build-sources.sh
+
+CMD ['sh']
