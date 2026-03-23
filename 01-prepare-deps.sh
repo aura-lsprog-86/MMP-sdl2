6e7e06c6c129691b9e427e4b06fe64a10dc7bc5d
Aura Lesse Programmer <aurasys.7608@zohomail.com>
2026-03-22 22:30:18 -0700

    Change progress density in toolchain dispatch

--- 01-prepare-deps.sh
+++ 01-prepare-deps.sh
@@ -2,7 +2,7 @@
 
 echo "Installing toolchain..."
 
-wget --progress=dot:mega -O - https://github.com/steward-fu/website/releases/download/miyoo-mini/mini_toolchain-v1.0.tar.gz \
+wget --progress=dot:giga -O - https://github.com/steward-fu/website/releases/download/miyoo-mini/mini_toolchain-v1.0.tar.gz \
   | bsdtar -xf- -C /opt mini/ prebuilt/ \
   && ls -1 /opt | grep -E '^(mini|prebuilt)$'
 
@@ -10,7 +10,7 @@ echo "Toolchain installed!"
 
 echo "Downloading sources..."
 
-wget --progress=dot:mega -O - https://github.com/steward-fu/sdl2/archive/refs/heads/master.zip \
+wget --progress=dot:giga -O - https://github.com/steward-fu/sdl2/archive/refs/heads/master.zip \
   | bsdtar -xf- -C . sdl2-master/ \
   && ls -1 . | grep -E 'sdl2-master$'
 
